d95deb1b6c77eac241ec79d91f5ec487b6456981
Gadrieldev <147134105+Felipedev-ga@users.noreply.github.com>
2024-01-29 14:16:11 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botao = document.querySelector('.btn-plataforma');
+
+const elementoPlataformas = document.querySelector('.btn-plataforma .plataformas')
+botao.addEventListener('click', () => {
+
+    elementoPlataformas.classList.toggle("ativo")
+
+
+                     ou
+                      
+    /*
+    if (botaoEstaAberto) {
+        elementoPlataformas.classList.remove('ativo')
+    }else{
+
+        elementoPlataformas.classList.add('ativo')
+    }
+    */
+})
